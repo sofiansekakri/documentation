baa34012e90ca5099fcbed1f523aa258a7b82743
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-10-10 09:47:12 +0100

    border-width

--- onboarding/main.js
+++ onboarding/main.js
@@ -27,14 +27,14 @@ var readerWriterDOM = `
 <div class="chose">
   <div class="section">
     <a class="writer">
-      <span class="wds-button" style="padding:40px;background-color:transparent;">
+      <span class="wds-button" style="padding:40px;background-color:transparent;border-width:3px;">
          <h2>I'm a writer</h2>
          <p></p>
       </span>
     </a>
   <div class="section">
     <a class="reader">
-      <span class="wds-button" style="padding:40px;background-color:transparent;">
+      <span class="wds-button" style="padding:40px;background-color:transparent;border-width:3px;">
          <h2>I'm a reader</h2>
          <p></p>
       </span>
