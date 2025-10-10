cb55f60f4cfb964daa88b55588aecc042d2929fb
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-10-10 18:26:51 +0100

    imma js copy this code from the o boarding script rq

--- rating/main.js
+++ rating/main.js
@@ -1,4 +1,11 @@
 // === [[Category:Internal]] ===
 mw.loader.using('mediawiki.api').then(() => {
-
+  function edit(title, content) {
+    return new mw.Api().postWithToken('csrf', {
+      action: 'edit',
+      title,
+      text: content,
+      summary: ''
+    });
+  }
 });
