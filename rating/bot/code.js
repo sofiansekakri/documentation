e8a1a08831e890242854be91ba178646547cc7ad
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-10-12 16:59:15 +0100

    Update code.js

--- rating/bot/code.js
+++ rating/bot/code.js
@@ -3,11 +3,12 @@ import fetchCookie from "fetch-cookie";
 const password = '';
 const botname = '';
 const api = "https://backrooms.fandom.com/api.php";
-function edit(title, content) {
-    .postWithToken('csrf', {
-      action: 'edit',
-      title: title,
-      text: content,
-      summary: ''
-    });
+
+
+async function getToken() {
+  const res = await fetch(API+`?action=query&meta=tokens&type=csrf&format=json`, {
+    headers: { Authorization: "Basic " + btoa(`${botname}:${password}`) }
+  })
+  const logintoken = await res.json()
+  return logintoken.query.tokens.csrftoken
 }
