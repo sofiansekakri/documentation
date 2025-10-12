017b38895d64c4308953f4ee050f866395d0feee
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-10-12 17:15:47 +0100

    Update code.js

--- rating/code.js
+++ rating/code.js
@@ -1,6 +1,7 @@
 // === [[Category:Internal]] ===
 mw.loader.using('mediawiki.api').then(() => {
   const page = mw.config.get('wgArticleId'); 
+  
   function edit(content) {
     return new mw.Api().postWithToken('csrf', {
       action: 'edit',
@@ -9,4 +10,14 @@ mw.loader.using('mediawiki.api').then(() => {
       summary: ''
     });
   }
+  function fetchpage() {
+      
+  }
+  function getpageratings {
+    fetchpage("MediaWiki:custom-ratingstorage.json").then(ratings => {
+      for(let i = 0; ratings.users.length; i++) 
+      {
+          if (rating.users[i].username === mw.config.get('wgUserName')) 
+      }
+    });
 });
