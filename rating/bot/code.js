b72e27f5cb4a36db03499bb66c4533ad7ecd70c1
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-10-12 16:51:47 +0100

    Update code.js

--- rating/bot/code.js
+++ rating/bot/code.js
@@ -2,6 +2,7 @@ import { CookieJar } from "tough-cookie";
 import fetchCookie from "fetch-cookie";
 const password = '';
 const botname = '';
+const api = "https://backrooms.fandom.com/api.php";
 function edit(title, content) {
     .postWithToken('csrf', {
       action: 'edit',
